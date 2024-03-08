78b109f214ea098acd80fa5d958b4f53597d4b3e
Christopher Holt <holt.christopher1@gmail.com>
2024-03-08 19:40:15 +1100

    changed signupform to be a functional component instead of a class, signup now works

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { signUp } from '../utilities/users-service';
 import './SignUpForm.css';
 import { Link } from 'react-router-dom';
@@ -9,71 +9,68 @@ import { RiLockPasswordFill } from 'react-icons/ri';
 import { CgRename } from 'react-icons/cg';
 import { useNavigate } from 'react-router-dom';
 
-export default class SignUpForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
-    confirm: '',
-    error: '',
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-      error: ''
+const SignUpForm = ({ setUser, showSignup, setShowSignup }) => {
+    const [userData, setUserData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: ''
     });
-  };
-
-  handleSubmit = async (e) => {
     const navigate = useNavigate();
-    e.preventDefault();
-    try {
-      const {name, email, password} = this.state;
-      const formData = {name, email, password};
-      // The promise returned by the signUp service
-      // method will resolve to the user object included
-      // in the payload of the JSON Web Token (JWT)
-      const user = await signUp(formData);
-      this.props.setUser(user);
-    } catch {
-      // An error occurred
-      // Probably due to a duplicate email
-      this.setState({ error: 'Sign Up Failed - Try Again' });
-    }
-  };
+    const [error, setError] = useState('');
 
-  render() {
-    const disable = this.state.password !== this.state.confirm;
+    const handleChange = (e) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value,
+            error: ''
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const user = await signUp(userData);
+            setUser(user);
+            navigate('/');
+        } catch {
+            setError('Sign Up Failed - Try Again');
+        }
+    }
+
+    const disable = userData.password !== userData.confirm;
+
     return (
-      <IconContext.Provider value={{ color: "white", size: "2.5em" }}>
-      <div>
-        <div className="signup-container">
-          <h1 style={{ marginBottom: '1em'}}>{this.props.showSignup ? 'Sign Up Page' : 'Log In Page'}</h1>
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <div className='username'>
-              <CgRename />
-              <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required placeholder='Username' />
+        <IconContext.Provider value={{ color: "white", size: "2.5em" }}>
+            <div>
+                <div className="signup-container">
+                    <h1 style={{ marginBottom: '1em' }}>{showSignup ? 'Sign Up Page' : 'Log In Page'}</h1>
+                    <form autoComplete="off" onSubmit={handleSubmit}>
+                        <div className='username'>
+                            <CgRename />
+                            <input type="text" name="name" value={userData.name} onChange={handleChange} required placeholder='Username' />
+                        </div>
+                        <div className='email'>
+                            <AiOutlineMail />
+                            <input type="email" name="email" value={userData.email} onChange={handleChange} required placeholder='Email' />
+                        </div>
+                        <div className='password'>
+                            <RiLockPasswordLine />
+                            <input type="password" name="password" value={userData.password} onChange={handleChange} required placeholder='Password' />
+                        </div>
+                        <div className='password-confirm'>
+                            <RiLockPasswordFill />
+                            <input type="password" name="confirm" value={userData.confirm} onChange={handleChange} required placeholder='Password Confirm' />
+                        </div>
+                        <button className='signup-button' type="submit" disabled={disable}>SIGN UP</button>
+                    </form>
+                    <p>Already have an account and want to login, click <Link onClick={() => setShowSignup(!showSignup)}>here</Link></p>
+                </div>
+                <p className="error-message">&nbsp;{error}</p>
             </div>
-            <div className='email'>
-              <AiOutlineMail />
-              <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required placeholder='Email'/>
-            </div>
-            <div className='password'>
-              <RiLockPasswordLine />
-              <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required placeholder='Password'/>
-            </div>
-            <div className='password-confirm'>
-              <RiLockPasswordFill />
-              <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required placeholder='Password Confirm' />
-            </div>
-            <button className='signup-button' type="submit" disabled={disable}>SIGN UP</button>
-          </form>
-          <p>Already have an account and want to login, click <Link onClick={() => this.props.setShowSignup(!this.props.showSignup)}>here</Link></p>
-        </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
-      </div>
-      </IconContext.Provider>
+        </IconContext.Provider>
     );
-  }
 }
+
+export default SignUpForm;
