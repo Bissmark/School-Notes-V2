ba7b5618eda1b31c40f58d15862d548a4c460a7a
Christopher Holt <holt.christopher1@gmail.com>
2024-03-18 11:39:27 +1100

    adding the render to the production env

--- src/components/CategoryDetail.jsx
+++ src/components/CategoryDetail.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const apiUrl = process.env.NODE_ENV === 'production'
+    ? 'https://school-notes-backend.onrender.com'
+    : 'http://localhost:5000';
+
 const CategoryDetail = () => {
     const [category, setCategory] = useState({});
     const [tasks, setTasks] = useState([]);
@@ -8,10 +12,10 @@ const CategoryDetail = () => {
 
     const getCategory = async () => {
         try {
-            const response = await fetch(`http://localhost:5000/categories/${id}`);
-            const categoryData = await response.json();
-            setCategory(categoryData);
-            setTasks(categoryData.tasks);
+            const response = await fetch(`http://${apiUrl}/categories/${id}`).then((res) => res.json());
+            //const categoryData = await response.json();
+            setCategory(response);
+            setTasks(response.tasks);
         } catch (error) {
             console.log(error);
         }
